1397cde905eb598a907061b699236f6dc48e0dec
MRGhidini <robert.ghidini@gmail.com>
2021-03-18 18:32:12 -0300

    First

---
+++ ServiceManagerDisabledSAPB1/ServiceManagerDisabled.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ServiceManagerDisabledSAPB1
+{
+    partial class ServiceManagerDisabled
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnrestore = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnrestore
+            // 
+            this.btnrestore.Location = new System.Drawing.Point(73, 65);
+            this.btnrestore.Name = "btnrestore";
+            this.btnrestore.Size = new System.Drawing.Size(143, 41);
+            this.btnrestore.TabIndex = 0;
+            this.btnrestore.Text = "Restore Services";
+            this.btnrestore.UseVisualStyleBackColor = true;
+            this.btnrestore.Click += new System.EventHandler(this.btnrestore_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "By MRGhidini";
+            // 
+            // ServiceManagerDisabled
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 210);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnrestore);
+            this.MaximizeBox = false;
+            this.Name = "ServiceManagerDisabled";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restore Service Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnrestore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ServiceManagerDisabledSAPB1/ServiceManagerDisabled.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceManagerDisabledSAPB1
+{
+    public partial class ServiceManagerDisabled : Form
+    {
+        public ServiceManagerDisabled()
+        {
+            InitializeComponent();
+        }
+
+        private void btnrestore_Click(object sender, EventArgs e)
+        {
+            string pathOrin = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\Local\SAP\SAP Business One";
+            string arcCreate = Properties.Resources.b1_current_user;
+
+            if (Directory.Exists(pathOrin))
+            {
+                try
+                {
+                   if(File.Exists(pathOrin + @"\b1-current-user.xml")) File.Delete(pathOrin + @"\b1-current-user.xml");
+                    CreateFileWrite(pathOrin + @"\b1-current-user.xml", Properties.Resources.b1_current_user);
+
+                    MessageBox.Show("Services successfully restored, restart your computer. ", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                catch(Exception err)
+                {
+                    MessageBox.Show("Erro: " + err, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }else
+            {
+                MessageBox.Show("SAP path does not exist on this machine or user: \n " + pathOrin, "Warning" ,MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        public static void CreateFileWrite(string path, string text)
+        {
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+
+        }
+    }
+}
